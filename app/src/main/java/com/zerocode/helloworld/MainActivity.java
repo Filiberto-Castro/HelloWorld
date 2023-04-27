b7c6fffca80589f5db2ce1e7f287052bce4c162b
Filiberto_Castro <fcastrob@miumg.edu.gt>
2023-04-26 21:09:31 -0600

    Android studio and hello world

--- app/src/main/java/com/zerocode/helloworld/MainActivity.java
+++ app/src/main/java/com/zerocode/helloworld/MainActivity.java
@@ -3,12 +3,27 @@ package com.zerocode.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nombre;
+    private TextView saludo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombre = findViewById(R.id.etName);
+        saludo = findViewById(R.id.tvSaludo);
+    }
+
+    public void btnSaludar(View view){
+
+        saludo.setText("Hola, " + nombre.getText().toString());
+
     }
 }
